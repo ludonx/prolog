af64ef114bce769e2d5ee204fd25c0ed65a6cf16
ludovik tekam <ludovik.tekam@edu.univ-paris13.fr>
2018-12-29 22:03:07 +0100

    arete + sommet + findall + open + pb_arete2

--- dm.pl
+++ dm.pl
@@ -1,30 +1,4 @@
 
-/*
-conc([],L,L).
-conc([X,L1],L2,[X,L3]) :- conc(L1,L2,L3).
-*/
-
-/*
-
-sommet_induit(NomF, Si,Motif) :-
-    read_dot_file(NomF, att_graph(S,A)),
-    member(Si,S),
-    Si=sommet(X,Lm),
-    member(Motif,Lm).
-
-sommet_induit(NomF, Si,Motif1,Motif2) :-
-    read_dot_file(NomF, att_graph(S,A)),
-    member(Si,S),
-    Si=sommet(X,Lm),
-    member(Motif1,Lm),
-    member(Motif2,Lm).
-
-    Si = sommet(b, [rock, folk, blues, jazz]) ;
-    Si = sommet(e, [rock, folk, blues]) ;
-    Si = sommet(n, [rock, folk, pop, blues]) ;
-    Si = sommet(r, [rock, folk, pop, blues]) ;
-
-*/
 /*member_liste(+ListeMotif,+Liste)
 permet de verifier si une liste de motif appartient a une autre Liste
 exemple :
@@ -37,27 +11,86 @@ s'interese uniquement à si la ListeMotif appartient a la liste
 */
 member_liste([],Liste).
 member_liste([X|ListeMotif],Liste):-member(X,Liste),member_liste(ListeMotif,Liste).
-liste(X,L, [X | L]).
+
 /* donne la liste des sommets qui contient le motif Motif
-  sommet_induit(+NomF,+ListeMotif,-Si,-X)
+  sommet_induit(+NomF,+ListeMotif,-L)
 */
-sommet_induit(NomF,ListeMotif,Si,X) :-
+sommet_induit(NomF,ListeMotif,L) :-
     read_dot_file(NomF, att_graph(S,A)),
-    member(Si,S),
-    Si=sommet(X,ListeMotifDuSommetX),
-    member_liste(ListeMotif,ListeMotifDuSommetX).
+    findall(X,
+      (
+      member(Si,S),
+      Si=sommet(X,ListeMotifDuSommetX),
+      member_liste(ListeMotif,ListeMotifDuSommetX)
+      )
+      ,L),
+      open('sommet.txt',write,Stream),
+      write(Stream,L),  nl(Stream),
+      close(Stream).
 
+sommet_induitSi(NomF,ListeMotif,L) :-
+    read_dot_file(NomF, att_graph(S,A)),
+    findall(Si,
+      (
+      member(Si,S),
+      Si=sommet(X,ListeMotifDuSommetX),
+      member_liste(ListeMotif,ListeMotifDuSommetX)
+      )
+      ,L),
+      open('sommetSi.txt',write,Stream),
+      write(Stream,L),  nl(Stream),
+      close(Stream).
+/* remarque, si on remplace le 1er parametre de findall
+par X,on obtient la liste des sommet L = [b, e, n, r]
+par Si, on obtient L = [sommet(b, [rock, folk, blues, jazz]), sommet(e, [rock, folk, blues]), sommet(n, [rock, folk, pop, blues]), sommet(r, [rock, folk, pop,
+blues])] ;*/
 /*
-exemple :
-?- sommet_induit("mougel_bis.dot",Si,[rock,blues])
-Si = sommet(b, [rock, folk, blues, jazz]) ;
-Si = sommet(e, [rock, folk, blues]) ;
-Si = sommet(n, [rock, folk, pop, blues]) ;
-Si = sommet(r, [rock, folk, pop, blues]) ;
+exemple d'utilisation :
+?- sommet_induit("mougel_bis.dot",[rock,blues],L).
+L = [sommet(b, [rock, folk, blues, jazz]), sommet(e, [rock, folk, blues]), sommet(n, [rock, folk, pop, blues]), sommet(r, [rock, folk, pop,
+blues])] ;
+false.
 
 */
 
-/* arete_induit(+NomF,+ListeMotif, -Ai,-X,-Y)  */
+/* arete_induit(+NomF,+ListeMotif, -Ai,-X,-Y)*/
+
+arete_induit(NomF,ListeMotif,L) :-
+  read_dot_file(NomF, att_graph(S,A)),
+  findall(Ai,
+    (
+    member(Ai,A),
+    Ai=arete(X, Y),
+    sommet_induit(NomF,ListeMotif,Ls),
+    member(X,Ls),
+    member(Y,Ls)
+    )
+    ,L),
+    open('arete.txt',write,Stream),
+    write(Stream,L),  nl(Stream),
+    close(Stream).
+arete_induit2(NomF,ListeMotif,L) :-
+  read_dot_file(NomF, att_graph(S,A)),
+  findall(Ai,
+    (
+    member(Ai,A),
+    Ai=arete(X, Y),
+    sommet_induitSi(NomF,ListeMotif,Ls),
+    member(sommet(X,Lm),Ls),
+    member(sommet(Y,Lm),Ls)
+    )
+    ,L),
+    open('arete2.txt',write,Stream),
+    write(Stream,L),  nl(Stream),
+    close(Stream).
+/*
+
+?- arete_induit2("mougel_bis.dot",[rock],L).
+L = [arete(a, c), arete(a, d), arete(c, d), arete(c, g), arete(n, r), arete(o, q)]
+
+?- arete_induit("mougel_bis.dot",[rock],L).
+L = [arete(a, b), arete(a, c), arete(a, d), arete(a, e), arete(a, k), arete(a, r), arete(b, c), arete(b, d), arete(..., ...)|...]
+
 arete_induit(NomF,ListeMotif, Ai,X,Y) :-
   read_dot_file(NomF, att_graph(S,A)),
   member(Ai,A),
@@ -67,3 +100,4 @@ arete_induit(NomF,ListeMotif, Ai,X,Y) :-
   open('hogwarts.txt',append,Stream),
   write(Stream,Ai),  nl(Stream),
   close(Stream).
+*/
